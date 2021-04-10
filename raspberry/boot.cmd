46e97abc4735fa0bc9f711f6b066bfd5f163cd2d
Thales Silva <thales.tas@gmail.com>
2021-04-09 23:24:41 -0400

    remove files related to tftp boot

--- raspberry/boot.cmd
+++
@@ -1,6 +0,0 @@
-setenv fdtfile bcm2835-rpi-zero-w.dtb
-mmc dev 0
-fatload mmc 0:1 ${kernel_addr_r} zImage
-fatload mmc 0:1 ${fdt_addr_r} ${fdtfile}
-setenv bootargs console=tty1 console=ttyAMA0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
-bootz ${kernel_addr_r} - ${fdt_addr_r}
